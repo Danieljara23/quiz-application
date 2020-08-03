d276f8d212c078fafcb3878295107205b838555f
Daniel Jaramillo <daniel.jaramillog23@gmail.com>
2020-08-03 07:02:56 -0500

    Theme added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,10 @@ import { cache } from 'emotion'
 import { ApolloProvider } from '@apollo/react-hooks'
 import { useApollo } from "../apollo/client";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { globalStyles } from '../shared/styles'
+import { globalStyles } from '../shared/styles';
+import { ThemeProvider } from 'emotion-theming';
+import { theme } from '../shared/theme';
+
 
 // const GRAPHQL_ENDPOINT = `http://localhost:3000/api/graphql`;
 const URL_GOOGLE_FONTS = "https://fonts.googleapis.com/css2?family=Muli:wght@400;600;700&display=swap";
@@ -13,31 +16,33 @@ const URL_GOOGLE_FONTS = "https://fonts.googleapis.com/css2?family=Muli:wght@400
 function MyApp({Component, pageProps, canonicalUrl}:any){
   const apolloClient = useApollo(pageProps.initialApolloState);
   return (
-    <ApolloProvider client={apolloClient}>
-      <CacheProvider value={cache}>
-        <Head>
-          <meta charSet="utf-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-          <meta name="HandheldFriendly" content="true" />
-          <link rel="canonical" href={canonicalUrl} />
-          <link rel="alternate" href={canonicalUrl} hrefLang="es" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-          <link rel="preload" as="style" href={URL_GOOGLE_FONTS} />
-        </Head>
-        {globalStyles}
-        <Global
-          styles={css`
-            @import url("${URL_GOOGLE_FONTS}");
+    <ThemeProvider theme={theme}>
+      <ApolloProvider client={apolloClient}>
+        <CacheProvider value={cache}>
+          <Head>
+            <meta charSet="utf-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+            <meta name="HandheldFriendly" content="true" />
+            <link rel="canonical" href={canonicalUrl} />
+            <link rel="alternate" href={canonicalUrl} hrefLang="es" />
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+            <link rel="preload" as="style" href={URL_GOOGLE_FONTS} />
+          </Head>
+          {globalStyles}
+          <Global
+            styles={css`
+              @import url("${URL_GOOGLE_FONTS}");
 
-            body * {
-              box-sizing: border-box;
-            }
-          `}
-        />
-        <Component {...pageProps} />
-        <div id ="modals-container"></div>
-      </CacheProvider>
-    </ApolloProvider>
+              body * {
+                box-sizing: border-box;
+              }
+            `}
+          />
+          <Component {...pageProps} />
+          <div id ="modals-container"></div>
+        </CacheProvider>
+      </ApolloProvider>
+    </ThemeProvider>
   )
 }
 
---
+++ shared/theme.tsx
@@ -0,0 +1,5 @@
+export const theme = {
+  colors: {
+    main_purple: '#5d1ad2ed'
+  }
+}
